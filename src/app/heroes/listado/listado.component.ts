7d0d8c2ca1cc5aa40826cd307c40afc5ee54ec8c
omarantonio16 <omar.antonio1695@gmail.com>
2021-07-10 13:54:11 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan América'];
+  heroeBorrado: string = '';
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
